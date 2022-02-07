5879d5d10a64f32d7621ab444add4299bb177498
Serhii Feshchuk <jeoparrdy@gmail.com>
2022-02-07 11:26:44 +0200

    Refactored structure

---
+++ fraud/src/main/java/com/bobocode/fraud/FraudCheckHistory.java
@@ -0,0 +1,34 @@
+package com.bobocode.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Long id;
+    private Long customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
